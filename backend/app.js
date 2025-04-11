35cbe1c428be9ae443a9841cfb92b3b7b956da4d
CDS2210 <colbysubramoney22@gmail.com>
2025-04-11 20:35:01 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,26 +2,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-const cors = require('cors');
-
-// Allow requests from your frontend
-app.use(cors({
+// CORS configuration
+const corsOptions = {
   origin: 'https://lively-dune-0a1933f1e.6.azurestaticapps.net',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
-}));
+};
+
+// Handle preflight requests
+app.options('*', cors(corsOptions));
+
+// Use CORS for all routes
+app.use(cors(corsOptions));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
